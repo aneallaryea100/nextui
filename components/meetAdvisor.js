9e898f47112b645af69b2a4f6885939673689edc
Aneal Laryea <aneallaryea100@gmail.com>
2023-12-28 21:38:19 +0000

    updatefix

--- components/meetAdvisor.js
+++ components/meetAdvisor.js
@@ -1,19 +1,27 @@
-import styles from '@styles/meetAdvisor.module.css'
+import styles from "@styles/meetAdvisor.module.css";
 
 function MeetAdvisor() {
   return (
-    <div className={styles.meetAdvisorWrapper}>
-      <div className={styles.meetAdvisorContainer}>
-        <div className={styles.advisorBox1}>
-            <h4 className={styles.advisorBox1Text}>Meet your new art advisor.</h4>
-            <button className={styles.advisorBtnDiscover}>Discover Tribals</button>
+    <div className="meetAdvisorWrapper mt-4">
+      <div className="meetAdvisorContainer w-full h-96 grid grid-rows-2 md:grid-cols-2">
+        <div className="advisorBox1 bg-orange-300 h-96 flex justify-center items-center">
+          <div className="flex flex-col items-center gap-4">
+            <div className="block">
+              <h4 className="advisorBox1Text block font-semibold text-2xl md:text-4xl">
+                Meet your new art advisor.
+              </h4>
+            </div>
+            <div>
+              <button className="advisorBtnDiscover text-center border-1 rounded-lg px-3 py-2 text-2xl hover:bg-black hover:text-white">
+                Discover Tribals
+              </button>
+            </div>
+          </div>
         </div>
-        <div className={styles.advisorBox2}>
-        </div>
+        <div className="advisorBox2 bg-advisorFooter w-full h-96 bg-no-repeat bg-center"></div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default MeetAdvisor
+export default MeetAdvisor;
